eb27abae19207dd5eead454ce2340934217858c6
liuhang <LHang1797618226@126.com>
2019-12-04 16:14:55 +0800

    257

---
+++ src/tree/num257.java
@@ -0,0 +1,43 @@
+package tree;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 257. 二叉树的所有路径
+ * 给定一个二叉树，返回所有从根节点到叶子节点的路径。
+ * 说明: 叶子节点是指没有子节点的节点。
+ * 示例:
+ * 输入:
+ *    1
+ *  /   \
+ * 2     3
+ *  \
+ *   5
+ * 输出: ["1->2->5", "1->3"]
+ * 解释: 所有根节点到叶子节点的路径为: 1->2->5, 1->3
+ */
+public class num257 {
+    /**
+     * 执行用时 :1 ms, 在所有 java 提交中击败了 100.00%的用户
+     * 内存消耗 : 36.4 MB , 在所有 java 提交中击败了97.17%的用户
+     */
+    public List<String> binaryTreePaths(TreeNode root) {
+        LinkedList<String> paths = new LinkedList();
+        construct_paths(root,"",paths);
+        return paths;
+    }
+
+    public void construct_paths(TreeNode root, String path, LinkedList<String> paths) {
+        if (root != null) {
+            path += Integer.toString(root.val);
+            if ((root.left == null) && (root.right == null))  // 当前节点是叶子节点
+                paths.add(path);  // 把路径加入到答案中
+            else {
+                path += "->";  // 当前节点不是叶子节点，继续递归遍历
+                construct_paths(root.left, path, paths);
+                construct_paths(root.right, path, paths);
+            }
+        }
+    }
+}
