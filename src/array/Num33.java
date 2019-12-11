027641e61b82ebc056c538ee857290ec72b3c99b
liuhang <LHang1797618226@126.com>
2019-12-11 21:04:38 +0800

    81

--- src/array/Num33.java
+++ src/array/Num33.java
@@ -1,32 +1,38 @@
 package array;
 
 /**
- * 31. 下一个排列
+ * 33. 搜索旋转排序数组
+ * 假设按照升序排序的数组在预先未知的某个点上进行了旋转。
+ * ( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+ * 搜索一个给定的目标值，如果数组中存在这个目标值，则返回它的索引，否则返回 -1 。
+ * 你可以假设数组中不存在重复的元素。
+ * 你的算法时间复杂度必须是 O(log n) 级别。
+ * 示例 1:
+ * 输入: nums = [4,5,6,7,0,1,2], target = 0
+ * 输出: 4
+ * 示例 2:
+ * 输入: nums = [4,5,6,7,0,1,2], target = 3
+ * 输出: -1
  *
  */
 public class Num33 {
-    public static void main(String[] arrs){
-        search(new int[]{4, 5, 6, 7, 0, 1, 2},0);
-    }
-
     /**
-     * 执行用时 :
-     * 1 ms
-     * , 在所有 java 提交中击败了
-     * 86.83%
-     * 的用户
-     * 内存消耗 :
-     * 36.8 MB
-     * , 在所有 java 提交中击败了
-     * 83.21%
-     * 的用户
+     * 由于题目说数字了无重复，举个例子：
+     * 1 2 3 4 5 6 7 可以大致分为两类，
+     * 第一类 2 3 4 5 6 7 1 这种，也就是 nums[start] <= nums[mid]。此例子中就是 2 <= 5。
+     * 这种情况下，前半部分有序。因此如果 nums[start] <=target<nums[mid]，则在前半部分找，否则去后半部分找。
+     * 第二类 6 7 1 2 3 4 5 这种，也就是 nums[start] > nums[mid]。此例子中就是 6 > 2。
+     * 这种情况下，后半部分有序。因此如果 nums[mid] <target<=nums[end]，则在后半部分找，否则去前半部分找。
      *
-     * 用二分法：分成四种情况讨论
+     * 执行用时 :
+     * 1 ms, 在所有 java 提交中击败了86.83%的用户
+     * 内存消耗 :
+     * 36.4 MB, 在所有 java 提交中击败了85.24%的用户
      * @param nums
      * @param target
      * @return
      */
-    public static int search(int[] nums, int target) {
+    public int search(int[] nums, int target) {
         if (nums == null || nums.length == 0) {
             return -1;
         }
